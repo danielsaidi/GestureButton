3502510d098f9f2f6bf981e5242e15027f405817
Daniel Saidi <daniel.saidi@gmail.com>
2024-09-02 09:50:48 +0200

    Make package require Swift 5.10

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -10,32 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var isPressed = false
-    
     var body: some View {
         VStack(spacing: 0) {
             ScrollView(.horizontal) {
-                HStack {
-                    ForEach(0...50, id: \.self) {
-                        Button("\($0)") {
-                            print("tap")
+                HStack(spacing: 50) {
+                    ForEach(0...50, id: \.self) { _ in
+                        ScrollViewGestureButton(
+                            pressAction: { print("Pressed") },
+                            releaseInsideAction: { print("Release: Inside") },
+                            releaseOutsideAction: { print("Release: Outside") },
+                            longPressAction: { print("Long Press") },
+                            doubleTapAction: { print("Double Tap") },
+                            repeatAction: { print("Repeat") },
+                            endAction: { print("Ended") }
+                        ) { isPressed in
+                            isPressed ? Color.green : Color.red
                         }
+                        .frame(width: 50)
                     }
                 }
-                .controlSize(.large)
-                .buttonStyle(.bordered)
-            }
-            GestureButton(
-                isPressed: $isPressed,
-                pressAction: { print("Pressed") },
-                releaseInsideAction: { print("Release: Inside") },
-                releaseOutsideAction: { print("Release: Outside") },
-                longPressAction: { print("Long Press") },
-                doubleTapAction: { print("Double Tap") },
-                repeatAction: { print("Repeat") },
-                endAction: { print("Ended") }
-            ) { isPressed in
-                isPressed ? Color.green : Color.red
             }
         }
     }
