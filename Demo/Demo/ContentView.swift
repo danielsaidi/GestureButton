b09603d9e0fd05f40e0a918f32defb7221e9ea61
Daniel Saidi <daniel.saidi@gmail.com>
2024-09-02 09:05:14 +0200

    Add demo project

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Daniel Saidi on 2024-09-02.
+//
+
+import GestureButton
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var isPressed = false
+    
+    var body: some View {
+        
+        GestureButton(
+            isPressed: $isPressed,
+            pressAction: { print("Pressed") },
+            releaseInsideAction: { print("Release: Inside") },
+            releaseOutsideAction: { print("Release: Outside") },
+            longPressAction: { print("Long Press") },
+            doubleTapAction: { print("Double Tap") },
+            repeatAction: { print("Repeat") },
+            endAction: { print("Ended") }
+        ) { _ in
+            isPressed ? Color.green : Color.red
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
