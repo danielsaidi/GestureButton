82d5a6acc8fd27aaabe1297a30060418be5cd450
Daniel Saidi <daniel.saidi@gmail.com>
2025-10-17 14:48:51 +0200

    Simplify demo

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -10,101 +10,78 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @FocusState var isEditing: Bool
-    
     @State var log = ""
-    
-    @StateObject var scrollState = GestureButtonScrollState()
-    
+    @State var repeatCount = 0
+
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading, spacing: 15) {
-                VStack(alignment: .leading) {
-                    Text("Plain Buttons")
-                    buttonStack(count: 3)
-                }
-                .padding(.horizontal)
-
-                VStack(alignment: .leading) {
-                    Text("ScrollView Buttons")
-                        .padding(.horizontal)
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        buttonStack(inScrollView: true)
-                            .padding(.horizontal)
-                    }
-                    .scrollClipDisabled()
-                    .scrollGestureState(scrollState) // <-- OBS!
-                }
-
-                logSection
-                    .padding(.horizontal)
-            }
-        }
-        .navigationTitle("GestureButton Demo")
-        .navigationBarTitleDisplayMode(.inline)
-        .task { isEditing = true }
-    }
-}
-
-private extension ContentView {
-    
-    func buttonStack(
-        count: Int = 50,
-        inScrollView: Bool = false
-    ) -> some View {
-        HStack(spacing: 20) {
-            ForEach(0..<count, id: \.self) { _ in
                 GestureButton(
-                    scrollState: inScrollView ? scrollState : nil, // <-- OBS!
                     pressAction: { log("Pressed") },
                     releaseInsideAction: { log("Release: Inside") },
                     releaseOutsideAction: { log("Release: Outside") },
                     longPressAction: { log("Long Press") },
                     doubleTapAction: { log("Double Tap") },
-                    // repeatAction: { log("Repeat") },             // Will generate a lot of logs
-                    // dragStartAction: { logDragValue("Start", $0) },
+                    repeatAction: { logRepeat() },
+                    dragStartAction: { logDragValue("Started", $0) },
                     // dragAction: { logDragValue("Move", $0) },    // Will generate a lot of logs
-                    // dragEndAction: { logDragValue("End", $0) },
-                    endAction: { log("Ended") }
+                    dragEndAction: { logDragValue("Ended", $0) },
+                    endAction: { log("\nEnded") }
                 ) { isPressed in
-                    Button("Button") {}
-                        .tint(buttonColor(isPressed))
-                        .buttonStyle(.borderedProminent)
-                        .controlSize(.large)
+                    buttonColor(isPressed)
+                        .overlay(Text(isPressed ? "Pressed!" : "Button"))
+                        .compositingGroup()
+                        .foregroundStyle(.white)
+                        .clipShape(.rect(cornerRadius: 10))
                         .shadow(radius: 1, y: 1)
+                }
+                Text("Log:")
+                    .font(.caption)
+                TextField("", text: $log, axis: .vertical)
+                    .disabled(true)
+                    .textFieldStyle(.roundedBorder)
+                    .lineLimit(8, reservesSpace: true)
+            }
+            .padding(.horizontal)
+            .navigationTitle("GestureButton Demo")
+            .safeAreaInset(edge: .bottom) {
+                Button("Clear log") {
+                    log = ""
                 }
             }
         }
     }
+}
 
-    func buttonColor(_ isPressed: Bool) -> AnyGradient {
-        isPressed ? Color.green.gradient : Color.accentColor.gradient
-    }
+private extension ContentView {
 
-    @ViewBuilder
-    var logSection: some View {
-        VStack {
-            TextField("", text: $log, axis: .vertical)
-                .focused($isEditing)
-                .lineLimit(5, reservesSpace: true)
-
-            Button("Clear log") {
-                log = ""
-            }
-        }
-        .buttonStyle(.borderedProminent)
-        .textFieldStyle(.roundedBorder)
+    func buttonColor(_ isPressed: Bool) -> Color {
+        isPressed ? .green : .accentColor
     }
 }
 
 private extension ContentView {
 
     func log(_ text: String) {
-        log = log + text + "\n"
+        log = text + "\n" + log
     }
 
     func logDragValue(_ event: String, _ value: DragGesture.Value) {
         log("Drag \(event): \(value.location.x.rounded()) \(value.location.y.rounded())")
+    }
+
+    func logRelease(_ place: String) {
+        log("Released \(place)")
+    }
+
+    func logRepeat() {
+        repeatCount += 1
+        guard repeatCount % 10 == 0 else { return }
+        log("Repeat \(repeatCount)")
+    }
+
+    func resetState() {
+        repeatCount = 0
     }
 }
 
